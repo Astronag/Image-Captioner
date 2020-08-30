7309f88aef5ddc970516930f54f8153f7317ecbd
Astronag <nagendrasomashekar@gmail.com>
2020-08-30 14:32:55 +0530

    Created tokenizer

--- ImageCaptioner.py
+++ ImageCaptioner.py
@@ -16,3 +16,11 @@ image_model = tf.keras.applications.InceptionV3(include_top=False, weights='imag
 new_input = image_model.input
 hidden_layer = image_model.layers[-1].output
 image_features_extract_model = tf.keras.Model(new_input, hidden_layer)
+tokenizer = pd.read_pickle('tokenizer.pkl')
+
+def load_image(image_path):
+    img = tf.io.read_file(image_path)
+    img = tf.image.decode_jpeg(img, channels=3)
+    img = tf.image.resize(img, (224, 224))
+    img = tf.keras.applications.inception_v3.preprocess_input(img)
+    return img, image_path
